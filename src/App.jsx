7f412c97bfa47ef0e9fe0f578e7a3086c1ab24fc
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-07-11 16:46:20 -0300

    feat: adjust files to eslint standards

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1>ibrahim borba</h1>
+      <nav>
+        <p>Projects</p>
+      </nav>
+    </>
+  );
+}
+
+export default App;
