35b82bbb3bf55d6e9b9e24e195e8c21f8d0bb5eb
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-07-14 15:55:40 -0300

    feat: add GlobalStyles

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalTheme from './styles/GlobalTheme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <ThemeProvider theme={GlobalTheme}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+    ,
+  </ThemeProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/styles/GlobalTheme.js
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material/styles';
+
+const GlobalTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#3949ab',
+    },
+  },
+});
+
+export default GlobalTheme;
