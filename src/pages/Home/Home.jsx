f106f3f0295b71a74b30e8c522c4e27b9a29eeaa
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-07-27 15:35:33 -0300

    style: change home image cover

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,6 @@ import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-// import CardMedia from '@mui/material/CardMedia';
 import { Footer } from '../../components/Footer';
 import { StyledBox } from '../../styles/GlobalTheme';
 import { Header } from '../../components/Header';
