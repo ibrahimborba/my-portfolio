ab6cdf7454ed4502fc3f9953baa6fc286016fe85
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-07-28 11:41:42 -0300

    refactor: change Home text to use Context

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Box from '@mui/material/Box';
 import CardMedia from '@mui/material/CardMedia';
 import Divider from '@mui/material/Divider';
@@ -10,8 +10,11 @@ import { Header } from '../../components/Header';
 import { ProjectsList } from '../../components/ProjectsList';
 import { AvaiableAt } from '../../components/AvaiableAt';
 import cover from '../../images/cover.png';
+import { ContentContext } from '../../context/ContentContext';
 
 function Home() {
+  const { content: { home } } = useContext(ContentContext);
+
   return (
     <>
       <Header />
@@ -39,10 +42,7 @@ function Home() {
           />
           <Box sx={{ p: { md: 6, sm: 2, xs: 0 } }}>
             <Typography variant="subtitle1" gutterBottom sx={{ maxWidth: '520px', mb: 3 }}>
-              {/* eslint-disable-next-line max-len -- Unable full text content   */}
-              Hi, welcome to my Portfolio! I&apos;m a
-              <Typography variant="subtitle2" display="inline"> Web Developer </Typography>
-              passionate about creating apps that impact people&apos;s lives.
+              {home.intro}
             </Typography>
             <Typography variant="h1">
               Ibrahim Borba
@@ -61,7 +61,7 @@ function Home() {
         </Stack>
         <StyledBox>
           <Typography variant="h2" gutterBottom>
-            Projects
+            {home.projectTitle}
           </Typography>
           <ProjectsList />
         </StyledBox>
