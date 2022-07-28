4234cf69b58881cecbc30dc6b3d78fc66254f32f
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-07-28 14:42:12 -0300

    feat: add NotFound page

--- src/context/data/contentEN.js
+++ src/context/data/contentEN.js
@@ -31,6 +31,12 @@ const content = {
     paragraph1: "Through my career, I've always been passionate about technology and how it can create a positive impact in people's lives. Which lead me to transition from architecture to development.",
     paragraph2: 'My goals are to always learn and improve as a programmer. And to create apps that will give a pleasant experience to its users and make their lives better.',
   },
+
+  notFound: {
+    title: 'Oops! That\'s unexpected.',
+    text: 'We couldn\'t find the page you are looking for.',
+    backTo: 'Back to Home',
+  },
 };
 
 export default content;
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,8 +1,60 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import CardMedia from '@mui/material/CardMedia';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import EastIcon from '@mui/icons-material/East';
+import { ContentContext } from '../../context/ContentContext';
+import notfound from '../../images/notfound.png';
 
 function NotFound() {
+  const { content: { notFound } } = useContext(ContentContext);
+
   return (
-    <h1>Not Found</h1>
+    <Stack
+      alignItems="center"
+      sx={{
+        p: 5,
+        height: '100vh',
+        marginBottom: { md: '50px', xs: '100px' },
+        justifyContent: { md: 'center', xs: 'end' },
+        backgroundImage: { md: 'none', xs: `url(${notfound})` },
+        backgroundSize: 'contain',
+        backgroundPosition: 'center top',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <Stack direction="row" alignItems="center">
+        <Box sx={{ p: 0, maxWidth: '550px' }}>
+          <Typography variant="h2">
+            {notFound.title}
+          </Typography>
+          <Typography variant="subtitle1" gutterBottom sx={{ mb: 2 }}>
+            {notFound.text}
+          </Typography>
+          <Button
+            variant="contained"
+            size="medium"
+            color="primary"
+            href="./"
+            endIcon={<EastIcon />}
+          >
+            {notFound.backTo}
+          </Button>
+        </Box>
+        <CardMedia
+          alt="not found"
+          component="img"
+          image={notfound}
+          sx={{
+            height: 500,
+            width: 350,
+            display: { xs: 'none', md: 'flex' },
+          }}
+        />
+      </Stack>
+    </Stack>
   );
 }
 
