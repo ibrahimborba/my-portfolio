ec4d5d6dc0f310d4d7fb9646602149b02f1498c2
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-08-16 19:36:57 -0300

    feat: add general content in pt

---
+++ src/context/data/contentPT.js
@@ -0,0 +1,46 @@
+const content = {
+  // Components
+  header: {
+    navProjects: 'Projetos',
+    navAbout: 'Sobre',
+    navContact: 'Contato',
+  },
+
+  footer: {
+    text: 'Feito com React e Material UI. Produção com Firebase',
+  },
+
+  avaiable: {
+    title: 'Você pode me encontrar em:',
+  },
+
+  projectCard: {
+    readMore: 'Veja mais',
+    madeWith: 'Feito com:',
+    visitHere: 'Visite aqui',
+  },
+
+  projectList: {
+    soon: 'Em breve...',
+  },
+
+  // Pages
+  home: {
+    intro: 'Olá, boas vindas ao meu Portfolio! Sou um Desenvolvedor Web apaixonado por criar aplicativos que impactem a vida das pessoas',
+    projectTitle: 'Projetos',
+  },
+
+  about: {
+    title: 'Sou um aspirante a Desenvolvedor Web que ama criar',
+    paragraph1: 'Ao longo da minha carreira, sempre fui apaixonado por tecnologia e como ela pode criar um impacto positivo na vida das pessoas. O que me levou a fazer uma transição de carreira de arquitetura para desenvolvimento.',
+    paragraph2: 'Meus objetivos são sempre aprender mais e me desenvolver como programador. E criar aplicativos que ofereçam uma experiência agradável para seus usuários e tornar a vida deles melhor.',
+  },
+
+  notFound: {
+    title: 'Opa! Isso não foi planejado.',
+    text: 'Não conseguimos encontrar a paǵina que você estava procurando.',
+    backTo: 'De volta para Home',
+  },
+};
+
+export default content;
